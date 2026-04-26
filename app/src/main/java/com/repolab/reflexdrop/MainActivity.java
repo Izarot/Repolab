f65ded4555bc2fbede46c39c67ca9e37808346b4
Izarot <sharananandanaraizarot@gmail.com>
2026-04-26 21:12:57 +0530

    Create app/src/main/java/com/repolab/reflexdrop/MainActivity.java

---
+++ app/src/main/java/com/repolab/reflexdrop/MainActivity.java
@@ -0,0 +1,24 @@
+package com.repolab.reflexdrop;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView web = new WebView(this);
+
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        web.loadUrl("file:///android_asset/ReflexDrop.html");
+
+        setContentView(web);
+    }
+}
