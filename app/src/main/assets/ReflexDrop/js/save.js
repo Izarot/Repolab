420abb35d030743f025ac9d2dfde5206bbb3a1a9
Izarot <sharananandanaraizarot@gmail.com>
2026-04-27 15:16:24 +0530

    Update save.js

--- app/src/main/assets/ReflexDrop/js/save.js
+++ app/src/main/assets/ReflexDrop/js/save.js
@@ -1,1 +1,17 @@
+let RD = {};
 
+function loadData(){
+RD = JSON.parse(localStorage.getItem("reflexdrop_save") || "{}");
+
+if(!RD.user) RD.user = "";
+if(!RD.coins) RD.coins = 0;
+if(!RD.bestScore) RD.bestScore = 0;
+if(!RD.bestCombo) RD.bestCombo = 0;
+if(!RD.games) RD.games = 0;
+if(!RD.theme) RD.theme = "theme-neon";
+if(!RD.inventory) RD.inventory = ["Default"];
+}
+
+function saveData(){
+localStorage.setItem("reflexdrop_save", JSON.stringify(RD));
+}
