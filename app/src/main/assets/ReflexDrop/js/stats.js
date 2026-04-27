9c07fb2011b538d3089d5082e671a072ec35f519
Izarot <sharananandanaraizarot@gmail.com>
2026-04-27 15:39:24 +0530

    Update stats.js

--- app/src/main/assets/ReflexDrop/js/stats.js
+++ app/src/main/assets/ReflexDrop/js/stats.js
@@ -1,1 +1,20 @@
+window.onload = ()=>{
 
+loadData();
+
+document.body.className = RD.theme;
+
+set("sGames", RD.games);
+set("sBest", RD.bestScore);
+set("sCombo", RD.bestCombo);
+set("sCoins", RD.coins);
+};
+
+function set(id,val){
+
+let el = document.getElementById(id);
+
+if(el){
+el.innerText = val;
+}
+}
