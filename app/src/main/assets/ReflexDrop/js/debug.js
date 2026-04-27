f16ff025b512d57999703ab87f29988bb7934410
Izarot <sharananandanaraizarot@gmail.com>
2026-04-27 15:41:28 +0530

    Update debug.js

--- app/src/main/assets/ReflexDrop/js/debug.js
+++ app/src/main/assets/ReflexDrop/js/debug.js
@@ -1,1 +1,38 @@
+function giveCoins(n){
 
+loadData();
+
+RD.coins += n;
+saveData();
+
+alert("Debug: +" + n + " coins");
+}
+
+function wipeSave(){
+
+localStorage.removeItem("reflexdrop_save");
+
+alert("Save wiped.");
+location.reload();
+}
+
+function maxStats(){
+
+loadData();
+
+RD.coins = 9999;
+RD.bestScore = 999;
+RD.bestCombo = 99;
+RD.games = 999;
+RD.chapter = 3;
+RD.inventory = [
+"Default",
+"Inferno",
+"Ultra",
+"Void"
+];
+
+saveData();
+
+alert("Debug max stats applied.");
+}
