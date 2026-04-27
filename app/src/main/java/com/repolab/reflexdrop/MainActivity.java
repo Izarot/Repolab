6410cb8f49b19f429909dc91fe88eb4996679b20
Izarot <sharananandanaraizarot@gmail.com>
2026-04-27 14:12:47 +0530

    Update MainActivity.java

--- app/src/main/java/com/repolab/reflexdrop/MainActivity.java
+++ app/src/main/java/com/repolab/reflexdrop/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
@@ -16,8 +17,12 @@ public class MainActivity extends Activity {
         WebSettings settings = web.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
 
-        web.loadUrl("file:///android_asset/ReflexDrop.html");
+        web.setWebViewClient(new WebViewClient());
+
+        web.loadUrl("file:///android_asset/ReflexDrop/index.html");
 
         setContentView(web);
     }
