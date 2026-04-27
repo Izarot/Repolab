bb9014121ab369258e3c67430d15eb8e091b2624
Izarot <sharananandanaraizarot@gmail.com>
2026-04-27 14:23:33 +0530

    Update main.js

--- app/src/main/assets/ReflexDrop/js/main.js
+++ app/src/main/assets/ReflexDrop/js/main.js
@@ -8,22 +8,6 @@ function updateCoins(){
 document.getElementById("coins").innerText = coins;
 }
 
-function openStory(){
-alert("Story Mode arriving next phase 👑");
-}
-
-function openInventory(){
-alert("Inventory arriving next phase 🎒");
-}
-
-function openAchievements(){
-alert("Achievements arriving next phase 🏆");
-}
-
-function openStore(){
-alert("Store arriving next phase 🛒");
-}
-
 function backMenu(){
 document.getElementById("gameOver").classList.add("hidden");
 document.getElementById("menu").style.display = "flex";
