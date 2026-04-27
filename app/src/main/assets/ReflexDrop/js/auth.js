071d82125bf2d9f313d9a68f0aeb9576673bf623
Izarot <sharananandanaraizarot@gmail.com>
2026-04-27 15:17:12 +0530

    Update auth.js

--- app/src/main/assets/ReflexDrop/js/auth.js
+++ app/src/main/assets/ReflexDrop/js/auth.js
@@ -1,1 +1,24 @@
+function loginUser(){
 
+let u = document.getElementById("username").value.trim();
+let p = document.getElementById("password").value.trim();
+
+if(!u || !p){
+alert("Enter username and password");
+return;
+}
+
+RD.user = u;
+saveData();
+
+updateHome();
+showHome();
+}
+
+function logoutUser(){
+
+RD.user = "";
+saveData();
+
+showLogin();
+}
