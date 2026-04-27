e848f7169d19ab59f8519142aa210402dbb1371e
Izarot <sharananandanaraizarot@gmail.com>
2026-04-27 15:16:49 +0530

    Update main.js

--- app/src/main/assets/ReflexDrop/js/main.js
+++ app/src/main/assets/ReflexDrop/js/main.js
@@ -1,18 +1,54 @@
-let coins = Number(localStorage.getItem("rd_coins") || 0);
-
-function saveCoins(){
-localStorage.setItem("rd_coins", coins);
-}
-
-function updateCoins(){
-document.getElementById("coins").innerText = coins;
-}
-
-function backMenu(){
-document.getElementById("gameOver").classList.add("hidden");
-document.getElementById("menu").style.display = "flex";
-}
-
 window.onload = ()=>{
-updateCoins();
+
+loadData();
+
+document.body.className = RD.theme;
+
+updateHome();
+
+if(RD.user){
+showHome();
+}else{
+showLogin();
+}
 };
+
+function goPage(name){
+location.href = name;
+}
+
+function showLogin(){
+showOnly("loginScreen");
+}
+
+function showHome(){
+showOnly("homeScreen");
+}
+
+function showGame(){
+showOnly("gameScreen");
+}
+
+function showOnly(id){
+
+document.querySelectorAll(".screen").forEach(s=>{
+s.classList.remove("active");
+});
+
+let el = document.getElementById(id);
+
+if(el) el.classList.add("active");
+}
+
+function updateHome(){
+
+let c = document.getElementById("coinCount");
+let s = document.getElementById("bestScore");
+let b = document.getElementById("bestCombo");
+let w = document.getElementById("welcomeText");
+
+if(c) c.innerText = RD.coins;
+if(s) s.innerText = RD.bestScore;
+if(b) b.innerText = RD.bestCombo;
+if(w) w.innerText = "Welcome, " + RD.user;
+}
